4d01426076372d19784c5ebb2242d7d8eb6edbc9
Thiago Aleixo <thiagoaleixo@pop-os.localdomain>
2023-09-25 17:05:01 -0300

    feat: Implementa rotas de sellers da api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\SellerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,13 +8,10 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Rotas de sellers
+ */
+Route::get('/sellers', [SellerController::class, 'index']);
+Route::post('/sellers', [SellerController::class, 'store']);
