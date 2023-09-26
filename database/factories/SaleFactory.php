eb3cbdd2fd2997683e9962982ff82c685e3a9dde
Thiago Aleixo <thiago.ataleixo@gmail.com>
2023-09-26 14:24:30 -0300

    feat: Adiciona factory de Sale

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Seller;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Seller>
+ */
+class SaleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'seller_id' => Seller::factory(),
+            'amount' => fake()->randomFloat(2, 0.01, 10000),
+        ];
+    }
+}
