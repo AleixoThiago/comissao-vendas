17c85c85b184c5f0a801b2f6916679656e571454
Thiago Aleixo <thiago.ataleixo@gmail.com>
2023-09-26 14:25:51 -0300

    feat: Implementa Model de Sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'seller_id',
+        'amount',
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
 }
