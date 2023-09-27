247ec011d3c18f9d56a453975f483293d2d6def4
Thiago Aleixo <thiago.ataleixo@gmail.com>
2023-09-27 15:15:14 -0300

    feat: Implementa rotas de autenticação e protege as demais

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\AdminAuthController;
 use App\Http\Controllers\API\SaleController;
 use App\Http\Controllers\API\SellerController;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,14 +13,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 /**
- * Rotas de sellers
+ * Rotas de autenticação para administradores
  */
-Route::get('/sellers', [SellerController::class, 'index']);
-Route::get('/sellers/{id}', [SellerController::class, 'getSeller']);
-Route::post('/sellers', [SellerController::class, 'store']);
+Route::get('/', fn () => response()->json(null, Response::HTTP_NOT_IMPLEMENTED));
+Route::post('/admin/login', [AdminAuthController::class, 'login']);
+Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->middleware('auth:admins');
 
-/**
- * Rotas de sales
- */
-Route::get('/sales', [SaleController::class, 'index']);
-Route::post('/sales', [SaleController::class, 'store']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    /**
+     * Rota de confirmação de autenticação
+     */
+    Route::get('/auth', fn () => response()->json(null, Response::HTTP_OK));
+
+    /**
+     * Rotas de sellers
+     */
+    Route::get('/sellers', [SellerController::class, 'index']);
+    Route::get('/sellers/{id}', [SellerController::class, 'getSeller']);
+    Route::post('/sellers', [SellerController::class, 'store']);
+
+    /**
+     * Rotas de sales
+     */
+    Route::get('/sales', [SaleController::class, 'index']);
+    Route::get('/sales/{sellerId}', [SaleController::class, 'getSellerSales']);
+    Route::post('/sales', [SaleController::class, 'store']);
+});
